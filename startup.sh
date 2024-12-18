8fb726fa59b1a8396102066e43b87dd7b1033ab3
Daniel Murray <daniel.murray@digital.cabinet-office.gov.uk>
2024-12-18 16:31:37 +0000

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker build . --tag gds-way
+docker build . --tag digital-handbook
 
-docker run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it gds-way
+docker run --rm -p 4567:4567 -p 35729:35729 -v $(pwd):/usr/src/docs -it digital-handbook
